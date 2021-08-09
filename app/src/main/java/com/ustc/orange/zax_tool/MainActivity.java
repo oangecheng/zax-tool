14d7ffc6f8dda44901fd1b56be9b098ca267346c
chengzhi <chengzhi@kuaishou.com>
2021-08-09 20:48:04 +0800

    init project

---
+++ app/src/main/java/com/ustc/orange/zax_tool/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ustc.orange.zax_tool;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
